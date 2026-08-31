c781892097a8c1a90bcccba4905487e03a32d5b4
Joao Caram <caram@pucminas.br>
2026-08-31 20:44:58 -0300

    Teste pizza inicial

--- src/PizzaTest.java
+++ src/PizzaTest.java
@@ -1,20 +1,41 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PizzaTest {
+    Pizza pizza;
 
+    @BeforeEach
+    public void setUp(){
+        //Arrange
+        pizza = new Pizza();
+        pizza.adicionarIngredientes(4);
+    }
     @Test
     public void adicionaIngredientesCorretamente(){
-        //Arrange
-        Pizza pizza = new Pizza();
-
         //Act
-        int quantos = 
-            pizza.adicionarIngredientes(4);
+        int quantos = pizza.adicionarIngredientes(1);
+
+        //Assert
+        assertEquals(5, quantos);
+    }
+    
+    @Test
+    public void naoAdicionaIngredientesNegativos(){
+        //Act
+        int quantos = pizza.adicionarIngredientes(-5);
 
         //Assert
         assertEquals(4, quantos);
     }
-    
+
+    @Test
+    public void naoAcumulaIngredientesEmExcesso(){
+        //Act
+        int quantos = pizza.adicionarIngredientes(5);
+
+        //Assert
+        assertEquals(4, quantos);
+    }
 }
